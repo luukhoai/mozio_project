f8b4577fe51b0a8a2a49d4ba3c3d0ab38867bda9
neonampv <luukhoai@gmail.com>
2021-08-16 17:22:50 +0700

    update unittest for search_indexes

--- search_indexes/tests.py
+++ search_indexes/tests.py
@@ -4,16 +4,22 @@ from providers.tests.factories import ProviderFactory
 from polygons.tests.factories import PolygonFactory
 from search_indexes.documents.polygon import PolygonDocument
 from polygons.models import Polygon
+from providers.models import Provider
 
 
 class SearchTest(TestCase):
 
     def setUp(self):
         self.url = '/search/polygon/'
-        # self.polygon = Polygon(
-        #     name='Test', price=123, provider=self.provider,
-        #     geo={'type': 'Point', 'coordinates': [1.2, 2.3]})
-        self.polygon = PolygonFactory.build()
-        polygonDoc = PolygonDocument()
-        with patch('django_elasticsearch_dsl.documents.bulk') as mock:
-            polygonDoc.update(self.polygon)
+        provider = ProviderFactory()
+        self.polygon = PolygonFactory(provider=provider)
+
+    def test_search(self):
+        url = '{}?geo__geo_distance=10000km__12.04__-63.93'.format(self.url)
+        response = self.client.get(url).json()
+        assert response['count'] == 1
+
+    def test_search_outside_distance(self):
+        url = '{}?geo__geo_distance=10km__12.04__-63.93'.format(self.url)
+        response = self.client.get(url).json()
+        assert response['count'] == 0
